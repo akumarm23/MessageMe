36977037e907fc475c6259129a6b4aaf3e610563
akumarm23 <ak.muniswamy2023@gmail.com>
2025-01-20 16:07:10 -0500

    Add messages from UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
     before_action :require_user
 
     def index
+        @message = Message.new
         @messages = Message.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   get 'login', to: 'login#new'
   post 'login', to: 'login#create'
   get 'logout', to: 'login#destroy'
+
+  post 'message', to: 'messages#create'
   
   # Defines the root path route ("/")
   # root "posts#index"
