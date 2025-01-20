a8f4c65ae8228cdf0a5b927c6e3fe3e58f9bae35
akumarm23 <ak.muniswamy2023@gmail.com>
2025-01-20 15:36:29 -0500

    Updated flash messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,5 +9,10 @@ window.$ = jquery
 
 $(document).ready(function () {     
     $(".ui.dropdown").dropdown(); 
-});
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
+})
+
+
 
