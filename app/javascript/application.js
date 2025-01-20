788735049be6a216ea14d3c241dcab155bd57465
akumarm23 <ak.muniswamy2023@gmail.com>
2025-01-20 13:03:49 -0500

    Initial Commit before login link

---
+++ app/javascript/application.js
@@ -0,0 +1,13 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "./jquery/jquery-3.6.0.js"
+import "semantic-ui"
+import jquery from "jquery";
+window.jQuery = jquery
+window.$ = jquery
+
+$(document).ready(function () {     
+    $(".ui.dropdown").dropdown(); 
+});
+
